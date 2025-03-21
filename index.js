ccf5539fbca6a0ce291dcfb27dd4fa8e0916dceb
AneeshMSomayaji <aneeshsomayaji14@gmail.com>
2025-03-21 11:34:56 +0530

    Fixed CORS issue properly

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import queryRoutes from "./routes/queries.js";
+var cors = require('cors')
 
+app.use(cors()) 
 dotenv.config();
 const app = express();
 app.use(express.json());
